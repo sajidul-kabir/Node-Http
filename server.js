7cc1d3fcdda1c24814c3e9e7740775c1426fa1a0
Sajidul Kabir <sajidulkabiradib@gmail.com>
2023-03-03 20:45:11 +0600

    used lstat instead of access

--- server.js
+++ server.js
@@ -11,21 +11,17 @@ const server = http.createServer(async (req, res) => {
     }
 
     const filePath = __dirname + req.url;
-    await fs.promises.access(filePath);
-
+    const stats = await fs.promises.lstat(filePath, fs.constants.R_OK);
+    if (stats.isDirectory()) {
+      throw new Error("Directory, not a file");
+    }
     const stream = fs.createReadStream(filePath);
-    const stats = await fs.promises.stat(filePath, fs.constants.R_OK);
     let streamError = false;
 
     stream.on("error", (err) => {
       streamError = true;
-      if (err.code === "EISDIR") {
-        logger(req.method, req.url, 404);
-        myError(`Directory, not a file---${err.message}`, 404, res);
-      } else if (err.code === "EACCES") {
-        logger(req.method, req.url, 401);
-        myError(`Permission Denied---${err.message}`, 401, res);
-      }
+      logger(req.method, req.url, 500);
+      myError(`STREAM ERROR---${err.message}`, 500, res);
     });
     stream.on("open", () => {
       res.writeHead(200, {
@@ -37,19 +33,25 @@ const server = http.createServer(async (req, res) => {
       if (!streamError) {
         logger(req.method, req.url, 200);
       } else streamError = false;
-      //console.log('The pipe operation has been completed fully.');
     });
 
     stream.pipe(res);
   } catch (err) {
+    //console.log(err.message);
     if (err.message === "Wrong method") {
       logger(req.method, req.url, 405);
       myError(`Only GET method is allowed---${err.message}`, 405, res);
+    } else if (err.message === "Directory, not a file") {
+      logger(req.method, req.url, 404);
+      myError(`${err.message}`, 404, res);
     } else if (
-      err.message === `ENOENT: no such file or directory, access '${err.path}'`
+      err.message === `ENOENT: no such file or directory, lstat '${err.path}'`
     ) {
       logger(req.method, req.url, 404);
       myError(`Requested resource is not available---${err.message}`, 404, res);
+    } else if (err.code === "EPERM") {
+      logger(req.method, req.url, 401);
+      myError(`Access denied---${err.message}`, 401, res);
     } else {
       logger(req.method, req.url, 500);
       myError(`Internal Server Error---${err.message}`, 500, res);
